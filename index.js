021fbc75c5a1e0c0cae8d820fa8813accbf57a3b
Ayrton Sparling <ayrton@sparling.us>
2018-09-10 17:23:55 -0700

    listen on socket test

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const fs = require("fs");
+const http = require('http');
 
-fs.writeFileSync("/tmp/test", "Hey there!");
+// Create an HTTP server
+const srv = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  console.log(`got client`, req.headers);
+  res.end('okay');
+});
+srv.listen(11638);
