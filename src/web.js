42122a49cebcc9d7c33e14eccb1a5a93511ab355
Ayrton Sparling <ayrton@sparling.us>
2018-09-19 14:48:41 -0700

    Add JWT authentication and CORS

--- src/web.js
+++ src/web.js
@@ -9,6 +9,10 @@ const {
     OpenApiValidator
 } = require("express-openapi-validate");
 const jsYaml = require("js-yaml");
+const jwtExpress = require('express-jwt');
+const jwt = require('jsonwebtoken');
+const phraseWell = require('know-your-http-well').statusCodesToPhrases;
+const cors = require('cors');
 
 // Require global logger
 const Logger = require('./logger');
@@ -16,6 +20,9 @@ const Logger = require('./logger');
 // Read OpenAPI specification and create request validator
 const openApiDocument = jsYaml.safeLoad(fs.readFileSync(path.join(__dirname, 'api.yaml'), "utf-8"));
 const validator = new OpenApiValidator(openApiDocument);
+
+// This secret is used to create the JWT
+const SECRET = "vU$Y/+[D;:<XraqlZ/q`lIe~`;\"u2=^H_GEk,@xGY:K4(CMF,'|TSFZAAFM-As)";
 
 // This handles sending out error messages to the client
 // when there is an async error
@@ -27,16 +34,23 @@ const asyncHandler = fn =>
 
 // If an error is caught while processing a request we will notify the user
 function errorHandler(err, req, res, next) {
-    const status = err.statusCode || 500;
+    const status = err.statusCode || err.status || 500;
 
     // 500 (Internal Server Error) represents an error that shouldn't happen
     if (status === 500) {
-        Logger.error(err);
+        Logger.error(err.message);
     }
+    Logger.trace(err);
     res.status(status).send({
         error: err.toString()
     });
     next();
+}
+
+// Generate a secret that is unique to each user (almost)
+async function generateSecret(req) {
+    const clientIp = req.headers['X-Forwarded-For'] || req.connection.remoteAddress;
+    return clientIp + SECRET;
 }
 
 // Setup API Router ------------------------------------------------------------
@@ -51,39 +65,63 @@ module.exports = function initWeb(database) {
 
         // Allow requests from any domain
         // TODO: This shouldn't be * (unsecure)
-        res.header("Access-Control-Allow-Origin", "*");
-
-        // Need this to use swagger examples
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        // res.header("Access-Control-Allow-Origin", "*");
+        //
+        // // Need this to use swagger examples
+        // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, X-Forwarded-For, Content-Type, Accept");
 
         // Some trace logging for every incomming request
-        Logger.trace("Incomming request:", req.method, req.url, req.path);
+        Logger.trace("<- |", req.method, req.url, req.path);
+
         next();
     });
 
+    // Browsers require cors to be enabled when making cross domain requests
+    app.use(cors())
 
     // All /api/* calls should be handled by this api router
     const api = express.Router();
     app.use('/api', api);
 
-
     // Serve the api specification file on /api
     api.get('/', (req, res) => res.sendFile(path.join(__dirname, 'api.yaml')));
 
+    // Use a JWT middleware to validate a json web token containing authentication info
+    api.use((...args) => {
+        const req = args[0];
+        const next = args[args.length - 1];
+        try {
+
+            // Only apply security to pages the explicately request it in the API spec
+            for (const security of openApiDocument.paths[req.path][req.method.toLowerCase()].security) {
+
+                // Handle JWT Bearer authentication
+                if (security.BearerAuth) {
+                    return generateSecret(req).then(secret => {
+                        return jwtExpress({
+                            secret
+                        }).apply(this, args);
+                    });
+                }
+            }
+            next();
+        } catch (e) {
+            next();
+        }
+    });
+
+    // Convert multipart/form-data into json and accept no files
     api.use(formProcessor.none());
 
     // Try to parse incoming requests to json if they are ['Content-Type': 'application/json']
     api.use(express.json());
 
+    // Validate request with OpenAPI specification
     api.use((...args) => validator.validate(args[0].method.toLowerCase(), args[0].path).apply(this, args));
-
-    // api.all('/api/*', requireAuthentication);
-
-
 
     // - asyncHandler allows us to use an async function to handle requests
     // Handle user registration
-    api.post('/users', asyncHandler(async function register(req, res) {
+    api.post('/users', asyncHandler(async function register(req, res, next) {
 
         // Create a database User from the data provided
         let user = new database.models.User(req.body);
@@ -94,10 +132,41 @@ module.exports = function initWeb(database) {
         Logger.debug(`User registered:`, req.body.username);
 
         res.status(201).send();
+
+        next();
+    }));
+
+    api.post('/users/login', asyncHandler(async function login(req, res, next) {
+        var token = jwt.sign({
+            sub: 'mongodb-user-objectid',
+            permissions: ['user']
+        }, await generateSecret(req));
+
+        res.json({
+            token
+        });
+
+        next();
+    }));
+
+    api.head('/users/logout', asyncHandler(async function logout(req, res, next) {
+        res.send();
+
+        next();
     }));
 
     // Handle sending errors to clients
     api.use(errorHandler);
+
+    // Middleware to log all responses to the Logger
+    app.use(function initialHandler(req, res, next) {
+
+        // Some trace logging for every incomming request
+        const statusCode = res.statusCode || res.status;
+        Logger.trace("-> |", statusCode, phraseWell[statusCode]);
+
+        next();
+    });
 
 
     if (process.env.TE_SSL_CERT_PATH && process.env.TE_SSL_KEY_PATH) {
