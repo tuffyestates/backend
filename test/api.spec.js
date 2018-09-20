cd94f72310c71e1cbbb59f7ebe42b9d16e57a416
Ayrton Sparling <ayrton@sparling.us>
2018-09-20 11:02:23 -0700

    Add api formatter

--- test/api.spec.js
+++ test/api.spec.js
@@ -1,11 +1,18 @@
+const fs = require('fs');
+
 const SwaggerParser = require('swagger-parser');
+const yaml = require('js-yaml');
 
 (async() => {
+    const spec = yaml.safeLoad(fs.readFileSync('./src/api.yaml'));
+
     try {
-        await SwaggerParser.validate('./src/api.yaml');
+        await SwaggerParser.validate(spec);
     } catch (e) {
         // eslint-disable-next-line no-console
         console.error('API Specifiation Error:', e.toString());
         process.exit(1);
     }
+
+    fs.writeFileSync('./src/api.yaml', yaml.safeDump(spec));
 })();
