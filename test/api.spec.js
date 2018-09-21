1a029048a5e825e22d2a8b837111c9ce72bfabe6
Ayrton Sparling <ayrton@sparling.us>
2018-09-21 15:10:15 -0700

    Fix merge and test

--- test/api.spec.js
+++ test/api.spec.js
@@ -3,16 +3,18 @@ const fs = require('fs');
 const SwaggerParser = require('swagger-parser');
 const yaml = require('js-yaml');
 
-(async() => {
-    const spec = yaml.safeLoad(fs.readFileSync('./src/api.yaml'));
-
+(async () => {
     try {
+        const spec = yaml.safeLoad(fs.readFileSync('./src/api.yaml'));
+
         await SwaggerParser.validate(spec);
+
+        fs.writeFileSync('./src/api.yaml', yaml.safeDump(spec, {
+            noRefs: true
+        }));
     } catch (e) {
         // eslint-disable-next-line no-console
         console.error('API Specifiation Error:', e.toString());
         process.exit(1);
     }
-
-    fs.writeFileSync('./src/api.yaml', yaml.safeDump(spec, {noRefs: true}));
 })();
