de81b25881cf39c7b819b1f3020bbae6c5f13a57
Van <vkyho96@gmail.com>
2018-10-26 21:32:34 -0700

    add auth middleware

--- src/web.js
+++ src/web.js
@@ -1,52 +1,52 @@
-import https from 'https';
-import fs from 'fs';
-import path from 'path';
+import https from "https";
+import fs from "fs";
+import path from "path";
 
-import express from 'express';
-import multer from 'multer';
+import express from "express";
+import multer from "multer";
 const formProcessor = multer();
-import {
-    OpenApiValidator
-} from "express-openapi-validate";
+import { OpenApiValidator } from "express-openapi-validate";
 import jsYaml from "js-yaml";
-import jwtExpress from 'express-jwt';
-import {statusCodesToPhrases} from 'know-your-http-well';
-import cors from 'cors';
-import get from 'lodash.get';
+import jwtExpress from "express-jwt";
+import { statusCodesToPhrases } from "know-your-http-well";
+import cors from "cors";
+import get from "lodash.get";
 
 // Require global logger
-import Logger from './logger';
-import controllers from './controllers';
-import {
-    generateSecret
-} from './utils';
+import Logger from "./logger";
+import controllers from "./controllers";
+import { generateSecret } from "./utils";
+
+import { loginRequired, ensureCorrectUser } from "auth.js";
 
 // Read OpenAPI specification and create request validator
-const openApiDocument = jsYaml.safeLoad(fs.readFileSync(path.join(__dirname, 'api.yaml'), "utf-8"));
+const openApiDocument = jsYaml.safeLoad(
+  fs.readFileSync(path.join(__dirname, "api.yaml"), "utf-8")
+);
 const validator = new OpenApiValidator(openApiDocument);
 
 // This handles sending out error messages to the client
 // when there is an async error
 const asyncHandler = fn =>
-    function asyncUtilWrap(req, res, next) {
-        const fnReturn = fn.apply(this, arguments);
-        return Promise.resolve(fnReturn).catch(next);
-    };
+  function asyncUtilWrap(req, res, next) {
+    const fnReturn = fn.apply(this, arguments);
+    return Promise.resolve(fnReturn).catch(next);
+  };
 
 // If an error is caught while processing a request we will notify the user
 function errorHandler(err, req, res, next) {
-    const status = err.status || err.name === 'ValidationError' ? 400 : 500;
+  const status = err.status || err.name === "ValidationError" ? 400 : 500;
 
-    // 500 (Internal Server Error) represents an error that shouldn't happen
-    if (status === 500) {
-        Logger.error(err.message);
-    }
-    Logger.trace(err);
-    res.status(status);
-    res.body = {
-        error: err.toString()
-    };
-    next();
+  // 500 (Internal Server Error) represents an error that shouldn't happen
+  if (status === 500) {
+    Logger.error(err.message);
+  }
+  Logger.trace(err);
+  res.status(status);
+  res.body = {
+    error: err.toString()
+  };
+  next();
 }
 
 // Setup API Router ------------------------------------------------------------
@@ -54,161 +54,148 @@ function errorHandler(err, req, res, next) {
 let appSingleton = null;
 
 export default function initWeb() {
+  if (appSingleton) {
+    return appSingleton;
+  }
 
-    if (appSingleton) {
-        return appSingleton;
-    }
+  // Create express app and create api path handler
+  let app = express();
 
-    // Create express app and create api path handler
-    let app = express();
+  // Middleware to log all requests to the Logger
+  app.use(function initialHandler(req, res, next) {
+    // Some trace logging for every incomming request
+    Logger.trace("<- |", req.method, req.url, req.path);
 
-    // Middleware to log all requests to the Logger
-    app.use(function initialHandler(req, res, next) {
+    next();
+  });
 
-        // Some trace logging for every incomming request
-        Logger.trace("<- |", req.method, req.url, req.path);
+  // Browsers require cors to be enabled when making cross domain requests
+  app.use(cors());
 
-        next();
-    });
-
-    // Browsers require cors to be enabled when making cross domain requests
-    app.use(cors());
-
-    // Serve static files
-    app.use('/static', express.static(process.env.TE_STATIC_DIRECTORY || '/srv/tuffyestates/production', {
+  // Serve static files
+  app.use(
+    "/static",
+    express.static(
+      process.env.TE_STATIC_DIRECTORY || "/srv/tuffyestates/production",
+      {
         fallthrough: false
-    }));
+      }
+    )
+  );
 
-    // All /api/* calls should be handled by this api router
-    const api = express.Router();
-    app.use('/api', api);
+  // All /api/* calls should be handled by this api router
+  const api = express.Router();
+  app.use("/api", api);
 
-
-    // Use a JWT middleware to validate a json web token containing authentication info
-    api.use((...args) => {
-        const req = args[0];
-        const next = args[args.length - 1];
-        try {
-
-            // Only apply security to pages the explicately request it in the API spec
-            for (const security of openApiDocument.paths[req.path][req.method.toLowerCase()].security) {
-
-                // Handle JWT Bearer authentication
-                if (security.BearerAuth) {
-                    return generateSecret(req).then(secret => {
-                        return jwtExpress({
-                            secret
-                        }).apply(this, args);
-                    });
-                }
-            }
-            next();
-        } catch (e) {
-            next();
+  // Use a JWT middleware to validate a json web token containing authentication info
+  api.use((...args) => {
+    const req = args[0];
+    const next = args[args.length - 1];
+    try {
+      // Only apply security to pages the explicately request it in the API spec
+      for (const security of openApiDocument.paths[req.path][
+        req.method.toLowerCase()
+      ].security) {
+        // Handle JWT Bearer authentication
+        if (security.BearerAuth) {
+          return generateSecret(req).then(secret => {
+            return jwtExpress({
+              secret
+            }).apply(this, args);
+          });
         }
-    });
+      }
+      next();
+    } catch (e) {
+      next();
+    }
+  });
 
-    // Convert multipart/form-data into json and accept no files
-    api.use(formProcessor.none());
+  // Convert multipart/form-data into json and accept no files
+  api.use(formProcessor.none());
 
-    // Try to parse incoming requests to json if they are ['Content-Type': 'application/json']
-    api.use(express.json());
+  // Try to parse incoming requests to json if they are ['Content-Type': 'application/json']
+  api.use(express.json());
 
-    // Validate request with OpenAPI specification
-    api.use((...args) => validator.validate(args[0].method.toLowerCase(), args[0].path).apply(this, args));
+  // Validate request with OpenAPI specification
+  api.use((...args) =>
+    validator
+      .validate(args[0].method.toLowerCase(), args[0].path)
+      .apply(this, args)
+  );
 
-    // Process API specification to add paths and their methods
-    for (const [path, methods] of Object.entries(openApiDocument.paths)) {
-        // path examples: '/', '/users', '/users/login'
+  // Process API specification to add paths and their methods
+  for (const [path, methods] of Object.entries(openApiDocument.paths)) {
+    // path examples: '/', '/users', '/users/login'
 
-        for (const method of Object.keys(methods)) {
-            // method examples: 'get', 'post', 'head'
+    for (const method of Object.keys(methods)) {
+      // method examples: 'get', 'post', 'head'
 
-            // Replace '/' with '.', add method, remove all leading '.s
-            const controllerPath = `${path.replace(/\//g, '.')}.${method}`.replace(/^\.+/, '');
+      // Replace '/' with '.', add method, remove all leading '.s
+      const controllerPath = `${path.replace(/\//g, ".")}.${method}`.replace(
+        /^\.+/,
+        ""
+      );
 
-            // Load controller
-            const controller = get(controllers, controllerPath);
+      // Load controller
+      const controller = get(controllers, controllerPath);
 
-            // Apply method to api
-            api[method](path, asyncHandler(controller));
-            Logger.trace(`Registered method:`, {
-                path,
-                method
-            });
-        }
+      // Apply method to api
+      api[method](path, asyncHandler(controller));
+      Logger.trace(`Registered method:`, {
+        path,
+        method
+      });
+    }
+  }
+
+  // Handle sending errors to clients
+  api.use(errorHandler);
+
+  // Middleware to log all responses to the Logger
+  app.use(function finalHandler(req, res, next) {
+    let statusCode = res.statusCode;
+
+    if (res.body) {
+      // If the body is json, send json and convert body to stringified json
+      if (typeof res.body === "object") {
+        res.type("application/json");
+        res.body = JSON.stringify(res.body);
+      }
+
+      // Send body
+      if (typeof res.body !== "boolean") res.send(res.body);
+      else res.send();
+    } else {
+      statusCode = 404;
     }
 
+    // Some trace logging for every incomming request
+    Logger.trace("-> |", statusCode, statusCodesToPhrases[statusCode]);
 
+    next();
+  });
 
-    // api.post('/properties', asyncHandler(async function login(req, res, next) {
+  if (process.env.TE_SSL_CERT_PATH && process.env.TE_SSL_KEY_PATH) {
+    Logger.info("Using SSL!");
+    app = https.createServer(
+      {
+        cert: fs.readFileSync(process.env.TE_SSL_CERT_PATH),
+        key: fs.readFileSync(process.env.TE_SSL_KEY_PATH)
+      },
+      app
+    );
+  }
 
-    //     let property = new database.models.Listing(req.body);
+  // Login Form POST
+  app.post("/login", (req, res) => {});
 
-    //     await user.save();
-    // }));
+  // Logout User
+  app.get("/logout", (req, res) => {
+    req.logout();
+    //res.redirect('/users/login');
+  });
 
-
-    // api.post('/properties/patch', asyncHandler(async function login(req, res, next) {
-
-    //     Listing.findByIdAndUpdate(req.params.id, req.body.Listing, function(err, updatedListing){
-    //         if(err){
-    //            // res.redirect();
-    //         } else {
-    //            // res.redirect();
-    //         }
-    //       });
-
-
-    // }));
-
-
-    // api.get('/properties/:id', asyncHandler(async function login(req, res, next) {
-
-
-    // }));
-
-    // Handle sending errors to clients
-    api.use(errorHandler);
-
-    // Middleware to log all responses to the Logger
-    app.use(function finalHandler(req, res, next) {
-
-        let statusCode = res.statusCode;
-
-        if (res.body) {
-
-            // If the body is json, send json and convert body to stringified json
-            if (typeof res.body === 'object') {
-                res.type('application/json');
-                res.body = JSON.stringify(res.body);
-            }
-
-            // Send body
-            if (typeof res.body !== 'boolean')
-                res.send(res.body);
-            else
-                res.send();
-
-        } else {
-            statusCode = 404;
-        }
-
-        // Some trace logging for every incomming request
-        Logger.trace("-> |", statusCode, statusCodesToPhrases[statusCode]);
-
-        next();
-    });
-
-
-    if (process.env.TE_SSL_CERT_PATH && process.env.TE_SSL_KEY_PATH) {
-        Logger.info("Using SSL!");
-        app = https.createServer({
-            cert: fs.readFileSync(process.env.TE_SSL_CERT_PATH),
-            key: fs.readFileSync(process.env.TE_SSL_KEY_PATH)
-        }, app);
-    }
-
-    appSingleton = app;
-    return app;
+  appSingleton = app;
+  return app;
 }
