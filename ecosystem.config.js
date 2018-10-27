1cb2c4b1565a5ce55598fa94ff28e012a15e2b43
Ayrton Sparling <ayrton@sparling.us>
2018-10-27 10:05:42 -0700

    Limit num of restart in production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ const name = `backend-${process.env.CI_ENVIRONMENT_NAME || 'production'}`;
 module.exports = {
     apps: [{
         name,
+        max_restarts: 3,
         script: 'index.js',
     }],
 };
