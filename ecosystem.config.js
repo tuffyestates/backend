3f07df1c22d312017dafca3ae900fab113ff9e4b
Ayrton Sparling <ayrton@sparling.us>
2018-11-02 14:04:50 -0700

    Add min_uptime to pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ const name = `backend-${process.env.CI_ENVIRONMENT_NAME || 'production'}`;
 module.exports = {
     apps: [{
         name,
+        min_uptime: 10000, // 10 second min_uptime
         max_restarts: 3,
         script: 'index.js',
     }],
