80e8314a554d822675678ed948b539a4b9fb8f67
Ayrton Sparling <ayrton@sparling.us>
2019-04-27 10:30:28 -0700

    Docker mongo import

--- test/tools/import.sh
+++ test/tools/import.sh
@@ -1,6 +1,6 @@
 echo "Importing users..."
-mongoimport --uri mongodb://localhost:27020/tuffyestates --mode upsert --collection users --file ../data/users.json --verbose --jsonArray
+docker-compose -f ../../docker/docker-compose-development.yml exec app mongoimport --uri mongodb://mongo:27017/tuffyestates --mode upsert --collection users --file /usr/src/app/test/data/users.json --verbose --jsonArray
 echo "Importing properties..."
-mongoimport --uri mongodb://localhost:27020/tuffyestates --mode upsert --collection properties --file ../data/properties.json --verbose --jsonArray
+docker-compose -f ../../docker/docker-compose-development.yml exec app mongoimport --uri mongodb://mongo:27017/tuffyestates --mode upsert --collection properties --file /usr/src/app/test/data/properties.json --verbose --jsonArray
 echo "Importing images... (This may take a while)"
 docker-compose -f ../../docker/docker-compose-development.yml exec app node -r esm /usr/src/app/test/tools/importImages.js
